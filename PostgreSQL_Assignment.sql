9c4fd294cf9580cafafdc4a4832d4f6ef3c9041f
arahman225 <upworkabdurrahman@gmail.com>
2025-05-26 16:11:57 +0600

    cleared some code

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,8 +1,11 @@
 -- Active: 1747648890294@@127.0.0.1@5432@conservation_db
+
+
 -- Problem - 1
 
 INSERT INTO rangers(name, region)
 VALUES('Derek Fox', 'Coastal Plains')
+
 
 
 -- Problem - 2 
@@ -13,20 +16,16 @@ SELECT COUNT(DISTINCT species_id) FROM sightings;
 -- Problem - 3
 
 SELECT * FROM sightings
-WHERE location LIKE '%Pass';
+WHERE location LIKE '%Pass%';
 
 -- Problem - 4
 
 
-SELECT name, COUNT(name) sighting_id
+SELECT rangers.name, COUNT(sightings.sighting_id) AS total_sigtings
 FROM sightings
 JOIN rangers ON sightings.ranger_id = rangers.ranger_id
-GROUP BY name;
+GROUP BY  rangers.name;
 
-SELECT name sighting_id
-FROM sightings
-JOIN rangers ON sightings.ranger_id = rangers.ranger_id
-GROUP BY name;
 
 
 -- Problem - 5
@@ -51,7 +50,7 @@ LIMIT 2
 
 UPDATE species
 SET conservation_status = 'Historic'
-WHERE DATE (discovery_date) < '1825-01-01'
+WHERE DATE (discovery_date) < '1800-01-01'
 
 
 -- Problem - 8
@@ -61,25 +60,20 @@ SELECT sighting_id, sighting_time FROM sightings;
 SELECT sighting_id,
 CASE 
     WHEN date_part('hour', sighting_time) BETWEEN 5 AND 12 THEN  'Morning'
-    WHEN date_part('hour', sighting_time) BETWEEN 12 AND 5 THEN  'Afternoon'
+    WHEN date_part('hour', sighting_time) BETWEEN 13 AND 17 THEN  'Afternoon'
+    WHEN date_part('hour', sighting_time) > 17 THEN  'Evening'
     ELSE  'Unknown'
 END AS time_of_day
 FROM sightings;
 
-SELECT DATE_PART('year', CURRENT_DATE);  
+
 
 -- Problem - 9
 
-SELECT name
-FROM sightings
-FULL JOIN rangers ON sightings.ranger_id = rangers.ranger_id
-WHERE sightings.ranger_id IS NULL;
 
--- DELETE FROM sightings
--- RIGHT JOIN rangers ON sightings.ranger_id = rangers.ranger_id
--- WHERE sightings.ranger_id IS NULL;
 
 DELETE FROM rangers
-WHERE NOT EXISTS(SELECT 1 FROM sightings WHERE sightings.ranger_id = rangers.ranger_id)
+WHERE NOT EXISTS
+(SELECT 1 FROM sightings WHERE sightings.ranger_id = rangers.ranger_id);
 
 
